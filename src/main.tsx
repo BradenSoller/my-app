ce6694c8bd1241474d8fc16e0e7f8e15a31d4429
Braden Soller <braden.soller18@gmail.com>
2025-02-17 14:05:07 -0600

    added provider

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './components/App/App.tsx'
+import { Provider } from 'react-redux'
+import store from './redux/store.ts' // Import your Redux store
 
 createRoot(document.getElementById('root')!).render(
+  <Provider store={store}>
   <StrictMode>
     <App />
-  </StrictMode>,
+    </StrictMode>,
+  </Provider>
 )
